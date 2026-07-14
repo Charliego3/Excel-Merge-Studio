c95385d92f081d1e7c5cb47195f366e0c797499a
Charlie <charlie.go.3@outlook.com>
2026-07-14 14:35:05 +0800

    Create SettingAction.svelte

---
+++ frontend/src/lib/components/SettingAction.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import * as ButtonGroup from "$lib/components/ui/button-group/index.js";
+    import { Button } from "$lib/components/ui/button/index.js";
+    import { getCurrentTableSelected } from "$lib/index.js";
+
+    let { selectedSheet = $bindable("") } = $props();
+
+    function settingHeader() {
+        let selected = getCurrentTableSelected(selectedSheet);
+        console.log(selected)
+    }
+
+    function hiddenRows() {
+
+    }
+
+    function hiddenCols() {
+
+    }
+</script>
+
+<ButtonGroup.Root class="text-gray-500">
+    <ButtonGroup.Root>
+        <Button onclick={settingHeader} size="xs" variant="outline" title="表头只能选择一行">设为表头</Button>
+        <Button onclick={hiddenRows} size="xs" variant="outline">隐藏行</Button>
+    </ButtonGroup.Root>
+    <ButtonGroup.Root>
+        <Button onclick={hiddenCols} size="xs" variant="outline">隐藏列</Button>
+    </ButtonGroup.Root>
+</ButtonGroup.Root>
