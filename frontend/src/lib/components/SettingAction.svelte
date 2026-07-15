d67b9f38d740e47d98f75d73efc88d2fdca014b7
Charlie <charlie.go.3@outlook.com>
2026-07-16 01:53:42 +0800

    buttons

--- frontend/src/lib/components/SettingAction.svelte
+++ frontend/src/lib/components/SettingAction.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import * as ButtonGroup from "$lib/components/ui/button-group/index.js";
     import { Button } from "$lib/components/ui/button/index.js";
     import { getCurrentTableSelected } from "$lib/index.js";
     import { SetHeader, SetMain, GetMain, DeleteColsAndRows } from "../../../bindings/merger/services/setting";
@@ -28,14 +27,9 @@
     }
 </script>
 
-<ButtonGroup.Root class="text-gray-500">
-    <ButtonGroup.Root>
-        <Button title={description} onclick={setMain} size="xs" variant="outline">设为主表 (Sheet)</Button>
-    </ButtonGroup.Root>
-    <ButtonGroup.Root>
-        <Button onclick={setHeader} size="xs" variant="outline" title="表头只能选择一行">设为表头</Button>
-    </ButtonGroup.Root>
-    <ButtonGroup.Root>
-        <Button onclick={deleteColsAndRows} size="xs" variant="outline">删除行和列</Button>
-    </ButtonGroup.Root>
-</ButtonGroup.Root>
+<div class="text-gray-500 flex justify-center gap-2">
+    <Button title={description} onclick={setMain} size="xs" variant="outline">设为主表 (Sheet)</Button>
+    <Button onclick={setHeader} size="xs" variant="outline" title="表头只能选择一行">设为表头</Button>
+    <Button onclick={setHeader} size="xs" variant="outline" title="主键列只能选择一列">设为主键列</Button>
+    <Button onclick={deleteColsAndRows} size="xs" variant="outline">删除行和列</Button>
+</div>
